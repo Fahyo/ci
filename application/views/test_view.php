5967e6e2cfb427eacc93ad0d45d0e581678baddb
Butsaba Tatit <yoyofahyo@gmail.com>
2016-11-26 16:35:18 +0700

    เพิ่ม bootstrap (home_view)

---
+++ application/views/test_view.php
@@ -0,0 +1,9 @@
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto at blanditiis dolores ducimus earum
+    enim esse est eum, excepturi exercitationem id iste provident quo reiciendis rem reprehenderit sint ullam.
+</div>
+<div>Ab accusamus autem consequuntur corporis cum cumque deserunt dolore eaque eius esse eum expedita facere in nulla
+    pariatur placeat praesentium quas quis repudiandae rerum saepe sapiente similique tenetur voluptas, voluptatibus!
+</div>
+<div>Accusantium alias aspernatur autem delectus earum eius eveniet ex expedita facere laboriosam laudantium minima,
+    nisi nostrum nulla provident quam quas qui quia quisquam quod repellat saepe sapiente tempore. Eveniet, porro?
+</div>
